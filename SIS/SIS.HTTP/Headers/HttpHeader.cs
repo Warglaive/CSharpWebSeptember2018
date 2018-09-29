ccba5e8069e50346b72fe48f65c2c63c2bfce982
warglaivee <warglaivegm@gmail.com>
2018-09-29 23:04:05 +0300

    up

---
+++ SIS/SIS.HTTP/Headers/HttpHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace SIS.HTTP.Headers
+{
+    public class HttpHeader
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public HttpHeader(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Key}: {this.Value}";
+        }
+    }
+}
---
+++ SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SIS.HTTP.Headers
+{
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            var value = GetHeader(header.Key);
+            this.headers.Add(header.Key, value);
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            return this.headers.Any(x => x.Key == key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            var header = this.headers.FirstOrDefault(x => x.Key == key).Value;
+            return header;
+        }
+    }
+}
