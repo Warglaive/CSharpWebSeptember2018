11a299043247b3ddf351327b939d795cc409aca6
warglaivee <warglaivegm@gmail.com>
2018-12-02 22:45:24 +0200

    merge to master

---
+++ ExamPrep/Panda/Apps/PandaWebApp/Controllers/Packages/PackagesController.cs
@@ -0,0 +1,21 @@
+﻿using PandaWebApp.ViewModels;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+
+namespace PandaWebApp.Controllers.Packages
+{
+    public class PackagesController : BaseController
+    {
+        public IHttpResponse Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IHttpResponse Create(PackageViewModel model)
+        {
+            //take info from model and take all recipients from dbContext
+            return null;
+        }
+    }
+}
