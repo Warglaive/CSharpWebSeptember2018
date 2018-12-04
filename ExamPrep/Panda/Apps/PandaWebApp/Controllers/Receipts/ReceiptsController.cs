ec82f1049e0de2996a50fa1ccb16732132d23fbf
warglaivee <warglaivegm@gmail.com>
2018-12-05 00:00:29 +0200

    Myreceipts page done

--- ExamPrep/Panda/Apps/PandaWebApp/Controllers/Receipts/ReceiptsController.cs
+++ ExamPrep/Panda/Apps/PandaWebApp/Controllers/Receipts/ReceiptsController.cs
@@ -1,12 +1,30 @@
-﻿using SIS.HTTP.Responses;
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using PandaWebApp.ViewModels;
+using SIS.HTTP.Responses;
 
-namespace PandaWebApp.Controllers
+namespace PandaWebApp.Controllers.Receipts
 {
     public class ReceiptsController : BaseController
     {
         public IHttpResponse Index()
         {
-            return this.View();
+            var receipts = this.ApplicationDbContext.Receipts.Include(x => x.Recipient).Where(x => x.Recipient.Username == this.User.Username);
+            var allReceipts = new AllMyReceiptsViewModel();
+            foreach (var receipt in receipts)
+            {
+                var currentReceipt = new ReceiptViewModel
+                {
+                    Id = receipt.Id,
+                    Fee = receipt.Fee,
+                    IssuedOn = receipt.IssuedOn.ToString(CultureInfo.InvariantCulture),
+                    Recipient = receipt.Recipient.Username
+                };
+                allReceipts.ReceiptViewModels.Add(currentReceipt);
+            }
+            return this.View(allReceipts);
         }
     }
 }
---
+++ ExamPrep/Panda/Apps/PandaWebApp/ViewModels/AllMyReceiptsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PandaWebApp.ViewModels
+{
+    public class AllMyReceiptsViewModel
+    {
+        public AllMyReceiptsViewModel()
+        {
+            this.ReceiptViewModels = new List<ReceiptViewModel>();
+        }
+        public ICollection<ReceiptViewModel> ReceiptViewModels { get; set; }
+    }
+}
