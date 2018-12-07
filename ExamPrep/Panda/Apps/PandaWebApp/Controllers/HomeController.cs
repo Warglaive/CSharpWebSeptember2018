6ef84d4058c9ff33df3fa8c435bfadb9a5487959
warglaivee <warglaivegm@gmail.com>
2018-12-07 17:29:40 +0200

    admin nav added and hello added

--- ExamPrep/Panda/Apps/PandaWebApp/Controllers/HomeController.cs
+++ ExamPrep/Panda/Apps/PandaWebApp/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using PandaWebApp.Models;
 using PandaWebApp.Models.Enums;
 using PandaWebApp.ViewModels;
 
