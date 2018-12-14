9042d1f5b2c079e1913e010c1bdc327ad77fdabb
warglaivee <warglaivegm@gmail.com>
2018-12-14 18:10:05 +0200

    MishMashExamPrep Start

---
+++ ExamPrep/MishMash/MishMashWebApp/MishMashWebApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MishMashWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
